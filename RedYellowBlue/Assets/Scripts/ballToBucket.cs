0b1207151f8b50fde44fe8d4a7b4d32046911684
Brian Ranck <brianlranck@yahoo.com>
2016-06-23 14:33:42 -0600

    Ball to bucket and ball respawn

---
+++ RedYellowBlue/Assets/Scripts/ballToBucket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballToBucket : MonoBehaviour {
+
+	//public Color ballColor;
+
+	public GameObject ball;
+	public int xPosition;
+	public int yPosition;
+	public int zPosition;
+
+	void OnTriggerEnter(Collider whichBall)
+	{
+
+		Destroy(whichBall.gameObject);
+
+		Instantiate (ball, new Vector3(xPosition,yPosition,zPosition), Quaternion.identity);
+
+	}
+		
+}
---
+++ RedYellowBlue/Assets/Scripts/initializeObjects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initializeObjects : MonoBehaviour {
+
+	Color red = Color.red;
+	//Color yellow = Color.yellow;
+	//Color blue = Color.blue;
+
+	public char ballColor;
+
+	// Use this for initialization
+	void Start () {
+
+		//if (ballColor = "R")
+		{	
+			Renderer rend = GetComponent<Renderer>();
+			//rend.material.shader = Shader.Find("Specular");
+			rend.material.SetColor("blueBall", red);
+		}
+	
+	}
+
+}
